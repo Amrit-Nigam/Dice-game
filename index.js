57ac736b54d183275e759578a00f8804cb09c375
Amrit Nigam <87406044+Vanquser@users.noreply.github.com>
2024-01-28 00:27:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function game(){
+    var number1 = Math.floor(Math.random()*6)+1;
+    var source1 = "/images/"+"dice"+number1+".png";
+
+    var number2 = Math.floor(Math.random()*6)+1;
+    var source2 = "/images/"+"dice"+number2+".png";
+
+    document.querySelectorAll("img")[0].setAttribute("src",source1);
+    document.querySelectorAll("img")[1].setAttribute("src",source2);
+
+    if(number1>number2){
+        document.querySelector("h1").innerHTML="Player 1 wins!!!";
+    }
+    else if(number1<number2){
+        document.querySelector("h1").innerHTML="Player 2 wins!!!";
+    }
+    else{
+        document.querySelector("h1").innerHTML="Its a Draw!!!";
+    }
+
+}
